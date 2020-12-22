854dca931d20bcc2fba2aeb7c2316e31c405748d
Stacy Toney <stacy.toney@protonmail.com>
2020-12-21 19:59:09 -0600

    old content brought over

---
+++ yesToJessBS4/Scripts/custom.js
@@ -0,0 +1,16 @@
+﻿///////////// Lightbox Code //////////////
+//  When a user clicks on a thumbnail image, display the lightbox container and insert an <img> tag, and allow user to clock off the lightbox
+$(".thumb").on("click", function () {
+
+    // display the lightbox container
+    $("#lightbox-container").fadeIn(500);
+
+    // insert an image tag
+    $("#lightbox-content").html('<img src="' + $(this).attr("src") + '" class="img-fluid" />');
+
+    // click off the lightbox
+    $("#lightbox-container").on("click", function () {
+        $(this).fadeOut(500);
+    });
+
+})
